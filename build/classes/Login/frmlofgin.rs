f640a5c5a8409183a9c9cfc1eff7a35bc48ad1d9
Brandon0902 <115058579+Brandon0902@users.noreply.github.com>
2024-09-03 13:56:31 -0600

    Add files via upload

---
+++ build/classes/Login/frmlofgin.rs
@@ -0,0 +1,7 @@
+login.frmlofgin$4
+login.frmlofgin$3
+login.frmlofgin$5
+login.frmlofgin$2
+login.frmlofgin$1
+login.frmlofgin
+login.frmlofgin$5$1
---
+++ build/classes/Login/frmregistro.rs
@@ -0,0 +1,5 @@
+login.frmregistro$2
+login.frmregistro$3$1
+login.frmregistro
+login.frmregistro$1
+login.frmregistro$3
